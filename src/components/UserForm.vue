deae01f29e7fd5642b503aa9bed2c2487adb9ba3
gurjeetsingh <g.singh@easternenterprise.com>
2022-06-08 06:34:53 +0530

    Adding form

---
+++ src/components/UserForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="p-2 mt-5">
+    <h1>{{ msg }}</h1>
+    <inputForm />
+  </div>
+</template>
+
+<script>
+import inputForm from './input'
+
+export default {
+  name: 'UserForm',
+  components: {
+    inputForm
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/components/input.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="submit">
+  <input v-model="username" class="p-2 border border-color-grey" placeholder="username" type="text"/>
+  <br>
+  <input v-model="password" class="mt-2 p-2 border border-color-grey" placeholder="password" type="password"/>
+    <br>
+    <button type="submit" class="mt-2 bg-white p-2 border border-color-grey">Submit</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    name: 'inputForm',
+    data(){
+      return {
+        username : "",
+        password : "",
+      }
+    },
+    methods:{
+      submit(){
+        console.log(this.username);
+        console.log(this.password);
+      }
+    }
+  }
+</script>
